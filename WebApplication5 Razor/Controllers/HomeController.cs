27abaaace0753ca9f22cc184739d39c692c6436b
Oleg <Oleg@OLEG_PC>
2016-09-27 02:17:43 +0300

    added index

--- WebApplication5 Razor/Controllers/HomeController.cs
+++ WebApplication5 Razor/Controllers/HomeController.cs
@@ -3,28 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication5_Razor.Models;
 
 namespace WebApplication5_Razor.Controllers
 {
     public class HomeController : Controller
     {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(myProduct);
         }
     }
 }
